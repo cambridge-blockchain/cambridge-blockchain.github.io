478c7710fe89b40573cf853e0f117a90354aae6f
Alex Oberhauser <alex@cambridge-blockchain.com>
2016-07-18 14:49:07 -0400

    Adding schema.org Organization annoation with JSON-LD syntax.

--- assets/js/gs.js
+++ assets/js/gs.js
@@ -6,3 +6,27 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-80927481-1', 'auto');
 ga('send', 'pageview');
 
+
+var el = document.createElement('script');
+el.type = 'application/ld+json';
+el.text = JSON.stringify({
+  "@context": "http://schema.org",
+  "@type": "Organization",
+  "url": "http://" + window.location.hostname,
+  "legalName": "Cambridge Blockchain, LLC",
+  "description": "Cambridge Blockchain puts control of personal identity data back in the hands of the end user. Our platform allows financial institutions to meet the strictest new data privacy rules, eliminate redundant identity compliance checks and improve the customer experience.",
+  "address": [{
+    "@type": "PostalAddress",
+    "addressCountry": "USA",
+    "addressLocality": "Cambridge",
+    "addressRegion": "MA",
+    "postalCode": "02142",
+    "streetAddress": "14th Floor, 1 Broadway"
+  }],
+  "contactPoint": [{
+    "@type": "ContactPoint",
+    "email": "support@cambridge-blockchain.com",
+    "contactType": "customer service"
+  }]
+});
+document.querySelector('body').appendChild(el);
