ad5045578f0b6e6b03001301a8559826fb1cad33
Alex Oberhauser <alex@cambridge-blockchain.com>
2016-07-20 16:35:19 -0400

    Fixing structural issues on the career page and including Google Analytics.

--- assets/js/gs.js
+++ assets/js/gs.js
@@ -7,27 +7,27 @@ ga('create', 'UA-80927481-1', 'auto');
 ga('send', 'pageview');
 
 
-var el = document.createElement('script');
-el.type = 'application/ld+json';
-el.text = JSON.stringify({
-  "@context": "http://schema.org",
-  "@type": "Organization",
-  "url": "http://" + window.location.hostname,
-  "legalName": "Cambridge Blockchain, LLC",
-  "description": "Cambridge Blockchain puts control of personal identity data back in the hands of the end user. Our platform allows financial institutions to meet the strictest new data privacy rules, eliminate redundant identity compliance checks and improve the customer experience.",
-  "address": [{
-    "@type": "PostalAddress",
-    "addressCountry": "USA",
-    "addressLocality": "Cambridge",
-    "addressRegion": "MA",
-    "postalCode": "02142",
-    "streetAddress": "14th Floor, 1 Broadway"
-  }],
-  "contactPoint": [{
-    "@type": "ContactPoint",
-    "email": "support@cambridge-blockchain.com",
-    "contactType": "support",
-    "url": "http://cambridge-blockchain.com/about/"
-  }]
-});
-document.querySelector('body').appendChild(el);
+// var el = document.createElement('script');
+// el.type = 'application/ld+json';
+// el.text = JSON.stringify({
+//   "@context": "http://schema.org",
+//   "@type": "Organization",
+//   "url": "http://" + window.location.hostname,
+//   "legalName": "Cambridge Blockchain, LLC",
+//   "description": "Cambridge Blockchain puts control of personal identity data back in the hands of the end user. Our platform allows financial institutions to meet the strictest new data privacy rules, eliminate redundant identity compliance checks and improve the customer experience.",
+//   "address": [{
+//     "@type": "PostalAddress",
+//     "addressCountry": "USA",
+//     "addressLocality": "Cambridge",
+//     "addressRegion": "MA",
+//     "postalCode": "02142",
+//     "streetAddress": "14th Floor, 1 Broadway"
+//   }],
+//   "contactPoint": [{
+//     "@type": "ContactPoint",
+//     "email": "support@cambridge-blockchain.com",
+//     "contactType": "support",
+//     "url": "http://cambridge-blockchain.com/about/"
+//   }]
+// });
+// document.querySelector('body').appendChild(el);
